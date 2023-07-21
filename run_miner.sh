8977f3e692958f54099d3309edd0d2a1b3c0bd75
Sethu Satheesh <118425907+WH1T3-E4GL3@users.noreply.github.com>
2023-07-21 19:43:45 +0530

    Update run_miner.sh

--- run_miner.sh
+++ run_miner.sh
@@ -2,6 +2,6 @@
 # For more usage info: hellminer --help
 
 # Example SSL URL
-# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 4
 
 ./hellminer -c stratum+tcp://na.luckpool.net:3960 -u RAY4c8b2P1JnuXLNfVguu7bs8XjUaqMCpL -p x --cpu 3
